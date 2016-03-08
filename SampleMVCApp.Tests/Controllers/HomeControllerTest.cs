965c463015d902c5b5c29c4f25def7310947f47e
Sachin Kumar Jain <sachin_kumar@dell.com>
2016-03-08 12:12:26 +0530

    test

--- SampleMVCApp.Tests/Controllers/HomeControllerTest.cs
+++ SampleMVCApp.Tests/Controllers/HomeControllerTest.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MvcApplication1;
 using MvcApplication1.Controllers;
 
 namespace SampleMVCApp.Tests.Controllers
 {
+
     [TestClass]
     public class HomeControllerTest
     {
